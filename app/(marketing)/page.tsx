c4d59efe566591c42ce3b31cef0e9bda0152cbc6
Neo <omkarkandale@gmail.com>
2024-04-14 12:55:43 +0530

    added content to marketing/main page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,13 @@
+import { Heading } from "./_components/heading";
+
 const MarketingPage = () => {
-	return <div>Hello this is a main starting page</div>;
+	return (
+		<div className="min-h-full flex flex-col">
+			<div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+				<Heading />
+			</div>
+		</div>
+	);
 };
 
 export default MarketingPage;
