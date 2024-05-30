3d2d0e012782b8708852ba82bfc9dcaf2be0c016
Neo <omkarkandale@gmail.com>
2024-05-30 09:04:54 +0530

    fix: onExpand

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -32,6 +32,13 @@ export const Item = ({
 }: ItemProps) => {
 	const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
+	const handleExpand = (
+		event: React.MouseEvent<HTMLDivElement, MouseEvent>
+	) => {
+		event.stopPropagation();
+		onExpand?.();
+	};
+
 	return (
 		<div
 			onClick={onClick}
@@ -46,7 +53,7 @@ export const Item = ({
 				<div
 					role="button"
 					className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-					onClick={() => {}}
+					onClick={handleExpand}
 				>
 					<ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
 				</div>
