83e839bce8c9cd08e2d7aae06bea8ef7bf935291
Neo <omkarkandale@gmail.com>
2024-05-30 10:03:49 +0530

    chore: created event for archive onClick on Trash

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -52,6 +52,7 @@ export const Item = ({
 	const { user } = useUser();
 	const router = useRouter();
 	const create = useMutation(api.documents.create);
+	const archive = useMutation(api.documents.archive);
 
 	const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
@@ -60,6 +61,18 @@ export const Item = ({
 	) => {
 		event.stopPropagation();
 		onExpand?.();
+	};
+
+	const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		event.stopPropagation();
+		if (!id) return;
+		const promise = archive({ id });
+
+		toast.promise(promise, {
+			loading: "Moving to trash...",
+			success: "Note moved to trash!",
+			error: "Failed to archive note.",
+		});
 	};
 
 	const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -127,7 +140,7 @@ export const Item = ({
 							side="right"
 							forceMount
 						>
-							<DropdownMenuItem onClick={() => {}}>
+							<DropdownMenuItem onClick={onArchive}>
 								<Trash className="h-4 w-4 mr-2" />
 								Delete
 							</DropdownMenuItem>
