a04c7f12cae88fa8332ce0e91896c0b1753c9b9f
Neo <omkarkandale@gmail.com>
2024-05-30 09:36:44 +0530

    feat(archive mutation): add a mutation to archive a note

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,6 +1,36 @@
 import { v } from "convex/values";
 
 import { mutation, query } from "./_generated/server";
+import { Id } from "./_generated/dataModel";
+
+export const archive = mutation({
+	args: { id: v.id("documents") },
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+
+		if (!identity) {
+			throw new Error("Not authenticated");
+		}
+
+		const userId = identity.subject;
+
+		const existingDocument = await ctx.db.get(args.id);
+
+		if (!existingDocument) {
+			throw new Error("Not found");
+		}
+
+		if (existingDocument.userId !== userId) {
+			throw new Error("Unauthorized");
+		}
+
+		const document = await ctx.db.patch(args.id, {
+			isArchived: true,
+		});
+
+		return document;
+	},
+});
 
 export const getSiderbar = query({
 	args: {
