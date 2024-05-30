875a9dafb5c4eb90f090c2ac9c123a31a41ff920
Neo <omkarkandale@gmail.com>
2024-05-30 09:23:59 +0530

    padding issue fixed

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -68,7 +68,7 @@ export const Item = ({
 		<div
 			onClick={onClick}
 			role="button"
-			style={{ paddingLeft: level ? `${level * 12 + 12}12px` : "12px" }}
+			style={{ paddingLeft: level ? `${level * 12 + 25}px` : "12px" }}
 			className={cn(
 				"group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
 				active && "bg-primary/5 text-primary"
