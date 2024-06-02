c63247ee0c358a07ac1865d5a93aec277be869b4
Neo <omkarkandale@gmail.com>
2024-06-02 22:57:49 +0530

    minor fix for kbd

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -120,7 +120,8 @@ export const Item = ({
 			<span className="truncate">{label}</span>
 			{isSearch && (
 				<kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-					<span className="text-xs">CTRL</span>K
+					<span className="text-xs">CTRL</span>
+					<span className="text-xs">K</span>
 				</kbd>
 			)}
 			{!!id && (
